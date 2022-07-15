b35d4787dd967dfda1c4a87020b1bea69f84bc99
spicymeat69 <109330382+spicymeat69@users.noreply.github.com>
2022-07-14 19:11:53 -1000

    top_secret.js
    never gonna open up

---
+++ top_secret.js
@@ -0,0 +1,9 @@
+print('never gonna give you up'"
+wait(.9)
+print('never gonna let you down')
+wait(.9)
+print('never gonna run around and desert you')
+wait(.9)
+print('never make you cry')
+wait(.9)
+print('never gonna say goodbye')
